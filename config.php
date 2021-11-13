0406e51bafb87f296f06d795c4e323754aee62f9
Adre Bagus Satria <adre.satria11@gmail.com>
2021-11-13 13:28:36 +0700

    n

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /** General Configuration */
 
 // Base Site URL`s (e.g., http://example.com/)
-define('BASE_URL', 'http://localhost:7777/akuntan/');
+define('BASE_URL', 'http://localhost:/akuntan/');
 
 // The hostname of your database server
 define('DB_HOSTNAME', 'localhost');
